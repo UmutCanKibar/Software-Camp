b2371c890e51d67a7e89cb8616b66e75028f699d
Umut Can Kibar <89220349+UmutCanKibar@users.noreply.github.com>
2021-08-29 00:44:28 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler." + musteri.MusteriAd + " " + musteri.MusteriSoyad + " " + "İsimli Müşteri Eklendi!!");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler." + musteri.MusteriAd + " " + musteri.MusteriSoyad + " " + "İsimli Müşteri Silindi!!");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri bilgileri Aşağıdaki gibidir");
+            Console.WriteLine("Müşteri Id       :" + musteri.MusteriId);
+            Console.WriteLine("Müşteri Adı      :" + musteri.MusteriAd);
+            Console.WriteLine("Müşteri Soyadı   :" + musteri.MusteriSoyad);
+            Console.WriteLine("Müşteri Numarası :" + musteri.MusteriTel);
+            Console.WriteLine("Müşteri Adresi :" + musteri.MusteriAdres);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAd = "Umutcan";
+            musteri1.MusteriSoyad = "Kibar";
+            musteri1.MusteriTel = 123456;
+            musteri1.MusteriAdres = "Turkey";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriAd = "Kenan";
+            musteri2.MusteriSoyad = "Kibar";
+            musteri2.MusteriTel = 345678;
+            musteri2.MusteriAdres = "Turkey";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 3;
+            musteri3.MusteriAd = "Çağrı";
+            musteri3.MusteriSoyad = "Kibar";
+            musteri3.MusteriTel = 567890;
+            musteri3.MusteriAdres = "Turkey";
+
+            MusteriManager musteri = new MusteriManager();
+            musteri.Listele(musteri1);
+
+            Console.WriteLine("--------------------------");
+
+            musteri.Add(musteri2);
+
+            Console.WriteLine("--------------------------");
+
+            musteri.Delete(musteri3);
+        }
+    }
+}
